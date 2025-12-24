30449a44aff7955d2ad273b7db993e456fc0884e
sanjaysubash <sanjaysubash038@gmail.com>
2025-12-24 12:11:58 +0530

    schl_form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="dark">
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
+        <link rel="icon" href="/logo2.png" type="image/png" />
       </head>
       <body
         className={`antialiased`}
